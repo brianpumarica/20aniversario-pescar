110446eb05777a5ca46650409beaf7d44da78e8e
Brian Pumarica <brianpumarica@gmail.com>
2023-08-21 14:33:46 -0300

    add ruta register

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -21,6 +21,13 @@ export const HomePage = () => {
                     </button>
                 </Link>
             </div>
+            <div className="mt-4">
+                <Link to="/register">
+                    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+                        Registrar
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 }
