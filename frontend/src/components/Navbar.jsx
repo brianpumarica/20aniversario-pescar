c9d3ea37a6865b567b677b70eb0f6dd476de0e56
Brian Pumarica <brianpumarica@gmail.com>
2023-08-22 16:28:39 -0300

    edit navbar menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,49 +1,138 @@
-import { Link, useNavigate} from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import PropTypes from 'prop-types'; // Import PropTypes for prop type validation
 import axios from 'axios';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-export const Navbar = ({ auth , setAuth}) => {
+const navigation = [
+    { name: 'Inicio', href: '/', current: true },
+    { name: 'Dashboard', href: '/Dashboard', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+export const Navbar = ({ auth, setAuth }) => {
     const backendURL = process.env.REACT_APP_BACKEND_URL;
     const navitage = useNavigate();
 
     const handleLogout = () => {
+        console.log(auth);
         axios.get(`${backendURL}/logout`)
-        .then(res=>{
-            console.log(res);
-            setAuth(false);
-            navitage('/login');
-        }).catch(err => console.log(err));
+            .then(res => {
+                console.log(res);
+                setAuth(false);
+                navitage('/login');
+            }).catch(err => console.log(err));
 
-         // Redirigir a la página de inicio de sesión
+        // Redirigir a la página de inicio de sesión
     };
 
     return (
-        <nav className="p-5 bg-white shadow md:flex md:items-center md:justify-between">
-            <div className="flex justify-between items-center">
-                <span className="text-2xl font-[Montserrat]">
-                    <Link to='/'>
-                        <img className="h-14 inline" src="https://i0.wp.com/www.pescar.org.ar/wp-content/uploads/2021/10/Home-Boceto-Pescar-2021-312x96-1.png?resize=312%2C96&ssl=1" alt="" />
-                    </Link>
-                </span>
-                <span className="text-3xl cursor-pointer mx-2 md:hidden block">
-                    {/* <!-- Al accionar en icono menu se ejecuta el this (llamada a funcion) --> */}
-                    <ion-icon name="menu-outline" onclick="Menu(this)"></ion-icon>
-                </span>
-            </div>
+        <Disclosure as="nav" className="bg-gray-800">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="relative ml-3">
+                                    <div>
+                                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <span className="absolute -inset-1.5" />
+                                            <span className="sr-only">Open user menu</span>
+                                            <img
+                                                className="h-8 w-8 rounded-full"
+                                                src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                                                alt=""
+                                            />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="/dashboard"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Mis datos
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <Link className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')} onClick={handleLogout}>Cerrar Sesión</Link>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
+                        </div>
+                    </div>
 
-            <ul className="add-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 md:flex">
-                <li className="mx-4 my-6 md:my-0">
-                    <Link to='/' className="text-xl hover:text-cyan-500 duration-500">Inicio</Link>
-                </li>
-                <li className="mx-4 my-6 md:my-0">
-                    {auth ? (
-                        <Link className="text-xl hover:text-cyan-500 duration-500" onClick={handleLogout}>Cerrar Sesión</Link>
-                    ) : (
-                        <Link to="/login" className="text-xl hover:text-cyan-500 duration-500">Iniciar Sesión</Link>
-                    )}
-                </li>
-            </ul>
-        </nav>
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block rounded-md px-3 py-2 text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
     );
 };
 Navbar.propTypes = {
