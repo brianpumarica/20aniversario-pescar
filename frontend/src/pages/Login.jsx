c5e1469d518805ce2184bae86f553809ce604b27
Brian Pumarica <brianpumarica@gmail.com>
2023-08-22 14:49:10 -0300

    edit style Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -29,12 +29,12 @@ function Login({ auth }) {
             await axios.post(`${backendURL}/login`, data)
                 .then(res => {
                     console.log(res);
-                // Agregar un retraso de 3 segundos antes de la navegación
-                setTimeout(() => {
-                    navigate('/dashboard');
-                    window.location.reload(); 
-                }, 1000);
-            });
+                    // Agregar un retraso de 3 segundos antes de la navegación
+                    setTimeout(() => {
+                        navigate('/');
+                        window.location.reload();
+                    }, 1000);
+                });
 
             Swal.fire({
                 icon: 'success',
@@ -55,38 +55,76 @@ function Login({ auth }) {
 
 
     return (
-
-        <div className="bg-gray-100 min-h-screen flex justify-center items-center">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-                <fieldset className="mb-4">
-                    <legend className="block text-gray-700 font-bold mb-2">Nombre de Usuario</legend>
-                    <input
-                        ref={user}
-                        type="text"
-                        className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        name="user"
-                        required
-                    />
-                </fieldset>
-
-                <fieldset className="mb-6">
-                    <legend className="block text-gray-700 font-bold mb-2">Password</legend>
-                    <input
-                        ref={password}
-                        type="password"
-                        className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        name="password"
-                        required
-                    />
-                </fieldset>
-
-                <input
-                    type="submit"
-                    value="Sign in"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-10 w-auto"
+                    src="https://i0.wp.com/www.pescar.org.ar/wp-content/uploads/2021/10/Home-Boceto-Pescar-2021-312x96-1.png?resize=312%2C96&ssl=1"
+                    alt="Your Company"
                 />
-            </form>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Ingresa a tu cuenta
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="user" className="block text-sm font-medium leading-6 text-gray-900">
+                            Nombre de Usuario
+                        </label>
+                        <div className="mt-2">
+              
+                            <input
+                                id="user"
+                                name="user"
+                                type="text"
+                                autoComplete="user"
+                                required
+                                ref={user}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Contraseña
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                ref={password}
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Inicia Sesion
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    No tenes cuenta?{' '}
+                    <a href="https://www.pescar.org.ar/contacto" target="_blank" rel="noopener noreferrer" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                        Contactate con nosotros
+                    </a>
+                </p>
+            </div>
         </div>
+
     )
 }
 
