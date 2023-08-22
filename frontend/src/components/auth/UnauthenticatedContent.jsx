93a77ba839609ff14a8fbfacd57dc77988aa6c5d
Brian Pumarica <brianpumarica@gmail.com>
2023-08-22 14:30:16 -0300

    edit style Unauthenticated Content

--- frontend/src/components/auth/UnauthenticatedContent.jsx
+++ frontend/src/components/auth/UnauthenticatedContent.jsx
@@ -1,24 +1,25 @@
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'; // Importa PropTypes
 
 // UnauthenticatedContent.jsx
 const UnauthenticatedContent = ({ message }) => {
-    
+
     return (
-        <div>
-            <p>{message}</p>
-            <div>
-                <Link to="/login">
-                    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                        Inicia Sesión
-                    </button>
-                </Link>
+
+        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <div className="text-center">
+                <p className="text-base font-semibold text-indigo-600">404</p>
+                <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{message}</h1>
+                <p className="mt-6 text-base leading-7 text-gray-600">Necesitas Logearte.</p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <a href="login" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Inicia Sesion</a>
+                    <a href="/" className="text-sm font-semibold text-gray-900">Inicio<span aria-hidden="true">&rarr;</span></a>
+                </div>
             </div>
-        </div>
+        </main>
 
     );
 }
 UnauthenticatedContent.propTypes = {
     message: PropTypes.string.isRequired, // Agrega la validación para 'message'
-  };
+};
 export default UnauthenticatedContent;
