04d0bdfb043c95664642dca83c1205c17fa74ddc
Brian Pumarica <brianpumarica@gmail.com>
2023-08-22 18:43:28 -0300

    add background image in Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -55,73 +55,81 @@ function Login({ auth }) {
 
 
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                    className="mx-auto h-10 w-auto"
-                    src="https://i0.wp.com/www.pescar.org.ar/wp-content/uploads/2021/10/Home-Boceto-Pescar-2021-312x96-1.png?resize=312%2C96&ssl=1"
-                    alt="Your Company"
-                />
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Ingresa a tu cuenta
-                </h2>
-            </div>
+        <div
+            className="h-screen 
+            * bg-[url('https://www.pescar.org.ar/wp-content/uploads/2021/10/Home-slider1.jpg')] 
+            * bg-cover bg-no-repeat bg-center"
+        >
+            <div className="flex min-h-full flex-1 flex-col justify-center px-20 py-12 lg:px-96">
+                <div className="text-center relative bg-white rounded-lg shadow-md flex flex-col items-center p-8 justify-center">
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="user" className="block text-sm font-medium leading-6 text-gray-900">
-                            Nombre de Usuario
-                        </label>
-                        <div className="mt-2">
-              
-                            <input
-                                id="user"
-                                name="user"
-                                type="text"
-                                autoComplete="user"
-                                required
-                                ref={user}
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
+                        <img
+                            className="mx-auto h-10 w-auto"
+                            src="https://i0.wp.com/www.pescar.org.ar/wp-content/uploads/2021/10/Home-Boceto-Pescar-2021-312x96-1.png?resize=312%2C96&ssl=1"
+                            alt="Your Company"
+                        />
+                        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                            Ingresa a tu cuenta
+                        </h2>
                     </div>
 
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Contraseña
-                            </label>
-                        </div>
-                        <div className="mt-2">
-                            <input
-                                id="password"
-                                ref={password}
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-6" onSubmit={handleSubmit}>
+                            <div className="text-left">
+                                <label htmlFor="user" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Nombre de Usuario
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="user"
+                                        name="user"
+                                        type="text"
+                                        autoComplete="user"
+                                        required
+                                        ref={user}
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
 
-                    <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Inicia Sesion
-                        </button>
-                    </div>
-                </form>
+                            <div>
+                                <div className="flex items-center justify-between text-left">
+                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Contraseña
+                                    </label>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        ref={password}
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
 
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    No tenes cuenta?{' '}
-                    <a href="https://www.pescar.org.ar/contacto" target="_blank" rel="noopener noreferrer" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Contactate con nosotros
-                    </a>
-                </p>
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Inicia Sesion
+                                </button>
+                            </div>
+                        </form>
+
+                        <p className="mt-10 text-center text-sm text-gray-500">
+                            No tenes cuenta?{' '}
+                            <a href="https://www.pescar.org.ar/contacto" target="_blank" rel="noopener noreferrer" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                                Contactate con nosotros
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 
