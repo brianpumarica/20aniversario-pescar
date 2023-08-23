7c3b419c3c22498dc47f60e407ec596ef232bf0e
Brian Pumarica <brianpumarica@gmail.com>
2023-08-23 01:56:00 -0300

    add component register

---
+++ frontend/src/components/register/RegisterForm.jsx
@@ -0,0 +1,201 @@
+import axios from 'axios'
+import { useState } from "react";
+
+export default function RegisterForm() {
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+    const [values, setValues] = useState({
+        nombreapellido: '',
+        telefono: '',
+        numeroinvitados: '',
+        empresa: '',
+        comida: 'Sin restricciones',
+        user: '',
+        password: ''
+    })
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setValues(prevValues => ({
+            ...prevValues,
+            [name]: value
+        }));
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(values);
+        axios.post(`${backendURL}/register`, values)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="space-y-12">
+                <div className="border-b border-gray-900/10 pb-12">
+                    <h2 className="text-base font-semibold leading-7 text-gray-900">Registrar nuevo usuario</h2>
+                    <p className="mt-1 text-sm leading-6 text-gray-600">
+                        El registro de usuario es para personas/entidades que han comprado una mesa o cubierto para la ceremonia.
+                    </p>
+                    <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-6">
+                        <div className="sm:col-span-4">
+                            <label htmlFor="nombreapellido" className="block text-sm font-medium leading-6 text-gray-900">
+                                Nombre y apellido
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input
+                                        type="text"
+                                        name="nombreapellido"
+                                        id="nombreapellido"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Ingrese nombre y apellido"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sm:col-span-4">
+                            <label htmlFor="telefono" className="block text-sm font-medium leading-6 text-gray-900">
+                                Teléfono
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input
+                                        type="text"
+                                        name="telefono"
+                                        id="telefono"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Ingrese el teléfono"
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sm:col-span-3">
+                            <label htmlFor="comida" className="block text-sm font-medium leading-6 text-gray-900">
+                                Comida
+                            </label>
+                            <div className="mt-2">
+                                <select
+                                    id="comida"
+                                    name="comida"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                                    onChange={handleChange}
+                                    required
+                                >
+                                    <option value="Sin restricciones" defaultValue>Sin restricciones</option>
+                                    <option value="vegetariano">Vegetariano</option>
+                                    <option value="vegano">Vegano</option>                                    <option value="celiaco">Celiaco</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="sm:col-span-4">
+                            <label htmlFor="numeroinvitados" className="block text-sm font-medium leading-6 text-gray-900">
+                                Número de invitados
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input
+                                        type="number"
+                                        name="numeroinvitados"
+                                        id="numeroinvitados"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Ingrese el número de invitados"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sm:col-span-4">
+                            <label htmlFor="empresa" className="block text-sm font-medium leading-6 text-gray-900">
+                                Empresa
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input
+                                        type="text"
+                                        name="empresa"
+                                        id="empresa"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Ingrese la empresa"
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sm:col-span-4">
+                            <h2 className="text-base font-semibold leading-7 text-gray-900">
+                                Datos de usuario único
+                            </h2>
+                        </div>
+                        <div className="sm:col-span-4">
+                            <label htmlFor="user" className="block text-sm font-medium leading-6 text-gray-900">
+                                Nombre de usuario
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input
+                                        type="text"
+                                        name="user"
+                                        id="user"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Ingrese el nombre de usuario"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sm:col-span-4">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Contraseña
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Ingrese la contraseña"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        {/* <div className="sm:col-span-4">
+                            <div className="relative flex gap-x-3">
+                                <div className="flex h-6 items-center">
+                                    <input
+                                        id="comments"
+                                        name="comments"
+                                        type="checkbox"
+                                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                    />
+                                </div>
+                                <div className="text-sm leading-6">
+                                    <label htmlFor="comments" className="font-medium text-gray-900">
+                                        Comments
+                                    </label>
+                                    <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
+                                </div>
+                            </div>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-6 flex items-center justify-center gap-x-6">
+                <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+                    mb-5"
+                >
+                    Registrar Usuario
+                </button>
+            </div>
+        </form>
+    );
+}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,86 +1,22 @@
-import axios from 'axios'
-import { useState } from "react";
 import AuthComponent from '../components/auth/authComponent';
 import UnauthenticatedContent from '../components/auth/UnauthenticatedContent';
+import RegisterForm from '../components/register/RegisterForm';
 
 export const Register = () => {
     const backendURL = process.env.REACT_APP_BACKEND_URL;
-    const { auth, message } = AuthComponent({ backendURL });
-    const [values, setValues] = useState({
-        nombreapellido: '',
-        telefono: '',
-        listaInvitados: '',
-        empresa: '',
-        comida: 'Sin restricciones',
-        user: '',
-        password: ''
-    })
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setValues(prevValues => ({
-            ...prevValues,
-            [name]: value
-        }));
-    };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(values);
-        axios.post(`${backendURL}/register`, values)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-    }
+    const { auth, message } = AuthComponent({ backendURL });   
 
     return (
-
         <div>
-            <div>
-                {auth ?
-                    <div>
-                        <h2>Registrar nuevo usuario</h2>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="nombreapellido"><strong>Nombre y apellido</strong></label>
-                                <input type="text" placeholder='Ingrese nombre y apellido' name='nombreapellido' onChange={handleChange} />
-                            </div>
-                            <div>
-                                <label htmlFor="telefono"><strong>Teléfono</strong></label>
-                                <input type="text" placeholder='Ingrese el teléfono' name='telefono' onChange={handleChange} />
-                            </div>
-                            <div>
-                                <label htmlFor="listaInvitados"><strong>Lista de Invitados</strong></label>
-                                <input type="number" placeholder='Ingrese la lista de invitados' name='listaInvitados' onChange={handleChange} />
-                            </div>
-                            <div>
-                                <label htmlFor="empresa"><strong>Empresa</strong></label>
-                                <input type="text" placeholder='Ingrese la empresa' name='empresa' onChange={handleChange} />
-                            </div>
-                            <div>
-                                <label htmlFor="comida"><strong>Comida</strong></label>
-                                <select name="comida" onChange={handleChange}>
-                                    <option value="Sin restricciones" defaultValue>Sin restricciones</option>
-                                    <option value="vegetariano">Vegetariano</option>
-                                    <option value="vegano">Vegano</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label htmlFor="user"><strong>Nombre de usuario</strong></label>
-                                <input type="text" placeholder='Ingrese el nombre de usuario' name='user' onChange={handleChange} />
-                            </div>
-                            <div>
-                                <label htmlFor="password"><strong>Contraseña</strong></label>
-                                <input type="password" placeholder='Ingrese la contraseña' name='password' onChange={handleChange} />
-                            </div>
-                            <button type='submit'>
-                                Registrar Usuario
-                            </button>
-                        </form>
-                    </div>
-                    :
-                    <div>
-                        <UnauthenticatedContent message={message} />
-                    </div>}
-            </div>
-
+            {auth ?
+            
+                <div>
+                    <RegisterForm/>
+                </div>
+                :
+                <div>
+                    <UnauthenticatedContent message={message} />
+                </div>}
         </div>
     );
 }
