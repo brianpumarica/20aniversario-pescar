b308b6a362d4bd1c414521321587e2f0ff76421c
Brian Pumarica <brianpumarica@gmail.com>
2023-08-24 00:37:43 -0300

    edit component navbar with auth state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,11 +5,14 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const navigation = [
+const navigationAuth = [
     { name: 'Inicio', href: '/', current: true },
     { name: 'Dashboard', href: '/Dashboard', current: false },
+    { name: 'Registrar', href: '/register', current: false },
 ]
-
+const navigationUnauth = [
+    { name: 'Inicio', href: '/', current: true },
+]
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
@@ -50,7 +53,8 @@ export const Navbar = ({ auth, setAuth }) => {
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
-                                        {navigation.map((item) => (
+                                    {auth ? (
+                                        navigationAuth.map((item) => (
                                             <a
                                                 key={item.name}
                                                 href={item.href}
@@ -62,7 +66,23 @@ export const Navbar = ({ auth, setAuth }) => {
                                             >
                                                 {item.name}
                                             </a>
-                                        ))}
+                                        ))
+                                    ) : (
+                                        navigationUnauth.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))
+                                    )}
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -112,20 +132,40 @@ export const Navbar = ({ auth, setAuth }) => {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block rounded-md px-3 py-2 text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
+                            {auth?(
+                                navigationAuth.map((item) => (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block rounded-md px-3 py-2 text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                ))
+                            )
+                            :(
+                                navigationUnauth.map((item) => (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block rounded-md px-3 py-2 text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                ))
+                            )
+
+                            }
                         </div>
                     </Disclosure.Panel>
                 </>
