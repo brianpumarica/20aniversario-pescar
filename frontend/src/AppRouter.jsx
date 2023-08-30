c79463ffba42f9b20bb4cb5072144871085de2d0
Brian Pumarica <brianpumarica@gmail.com>
2023-08-30 00:04:18 -0300

    refact AppRouter.jsx

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,62 +1,70 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { HomePage, CategoryPage, Register, } from './pages';
+import { HomePage, CategoryPage, Register } from './pages';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 
 const AppRouter = () => {
-    const [auth, setAuth] = useState(false);
-    const [rol, setRol] = useState('usuario');
-    const [message, setMessage] = useState('');
-    const backendURL = process.env.REACT_APP_BACKEND_URL;
+  const [auth, setAuth] = useState(false);
+  const [rol, setRol] = useState('usuario');
+  const [message, setMessage] = useState('');
+  const backendURL = process.env.REACT_APP_BACKEND_URL;
 
-    axios.defaults.withCredentials = true;
+  axios.defaults.withCredentials = true;
 
-    useEffect(() => {
-        axios.get(`${backendURL}/verify`)
-            .then(res => {
-                if (res.data.Status === "Success") {
-                    setMessage(res.data.Status);
-                    setRol(res.data.rol)
-                    setAuth(true);
-                    return axios.post(`${backendURL}/saveId`, { id: res.data.id });
-                } else {
-                    setAuth(false);
-                    setMessage(res.data.Error);
-                }
-            })
-            .then(response => {
-                if (response) {
-                    console.log(response.data);
-                    setAuth(true);
-                }
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    }, [backendURL,setAuth, setRol]);
+  useEffect(() => {
+    checkAuthentication();
+  }, [backendURL]);
 
-    return (
-        <Router basename="/">
-            <div className="min-h-screen">
-                <Navbar auth={auth} setAuth={setAuth} />
-                <Routes>
-                    <Route path='/' element={<HomePage auth={auth} />} />
-                    <Route path='/category/:category' element={<CategoryPage />} />
-                    <Route path='/login' element={<Login auth={auth} setAuth={setAuth}/>} />
-                    <Route
-                        path='/dashboard'
-                        element={<Dashboard auth={auth} message={message} />}
-                    />
-                    <Route path='/register' element={<Register rol={rol} />} />
-                </Routes>
-                <Footer />
-            </div>
-        </Router>
-    )
-}
+  const checkAuthentication = async () => {
+    try {
+      const response = await axios.get(`${backendURL}/verify`);
+
+      if (response.data.Status === 'Success') {
+        setMessage(response.data.Status);
+        setRol(response.data.rol);
+        setAuth(true);
+        saveIdToServer(response.data.id);
+      } else {
+        setAuth(false);
+        setMessage(response.data.Error);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setAuth(false);
+      setMessage('Error en el servidor');
+    }
+  };
+
+  const saveIdToServer = async (id) => {
+    try {
+      await axios.post(`${backendURL}/saveId`, { id });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <Router basename="/">
+      <div className="min-h-screen">
+        <Navbar auth={auth} setAuth={setAuth} />
+        <Routes>
+          <Route path="/" element={<HomePage auth={auth} />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="/login" element={<Login auth={auth} setAuth={setAuth} />} />
+          <Route
+            path="/dashboard"
+            element={<Dashboard auth={auth} message={message} />}
+          />
+          <Route path="/register" element={<Register rol={rol} />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
 export default AppRouter;
