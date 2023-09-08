8d27bde72d52f95e77fcea67c24e967e63f56437
Brian Pumarica <brianpumarica@gmail.com>
2023-09-08 10:08:04 -0300

    menu navbar works

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,58 +1,17 @@
-// import { useNavigate, Link } from 'react-router-dom';
-// import { useNavigate} from 'react-router-dom';
-// import PropTypes from 'prop-types'; // Import PropTypes for prop type validation
-// import axios from 'axios';
-// import { Fragment, useState } from 'react'
-// import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-// import Swal from 'sweetalert2';
 
-// const navigationAuth = [
-//     { name: 'Inicio', href: '/', current: true },
-//     { name: 'Dashboard', href: '/Dashboard', current: false },
-// ]
-
-const navigationAuthWithRol = [
+const navigation = [
     { name: 'Inicio', href: '/', current: true },
     { name: 'Trivia', href: '/#/category/Pescar', current: false },
     { name: 'Emprendimientos', href: '/#/emprendimientos', current: false },
+    { name: 'Estudiantes', href: '/#/estudiantes', current: false },
 ]
-// const navigationUnauth = [
-//     { name: 'Inicio', href: '/', current: true },
-//     { name: 'Iniciar Sesión', href: '/#/login', current: false },
-// ]
+
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
-// export const Navbar = ({ auth, setAuth , rol, setRol}) => {
 export const Navbar = () => {
-        // const backendURL = process.env.REACT_APP_BACKEND_URL;
-    // const navigate = useNavigate();
-    // const [auth, setAuth] = useState(true);
-    // const [rol, setRol] = useState('usuario');
-    // setAuth(true);
-    // setRol('usuario');
-    // const handleLogout = () => {
-    //     // axios.get(`${backendURL}/api/logout`)
-    //     //     .then(() => {
-    //     //         setAuth(false);
-    //     //         if (rol === 'admin') {
-    //     //             setRol('usuario'); // Set the role to 'usuario' only if it was 'admin'
-    //     //         }
-    //     //         Swal.fire({
-    //     //             icon: 'success',
-    //     //             text: '¡Sesión cerrada!',
-    //     //             timer: 1500, // Configura el tiempo en milisegundos
-    //     //             timerProgressBar: true
-    //     //         }).then(() => {
-    //     //             navigate('/login');
-    //     //         });
-    //     //     }).catch(err => console.log(err));
-
-    //     // Redirigir a la página de inicio de sesión
-    //     navigate('/login');
-    // };
 
     return (
         <Disclosure as="nav" className="bg-gray-800">
@@ -75,158 +34,52 @@ export const Navbar = () => {
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
-                                    {navigationAuthWithRol.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className={classNames(
-                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                'rounded-md px-3 py-2 text-sm font-medium'
-                                            )}
-                                            aria-current={item.current ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </a>
-                                    ))}
-                                    {/* {auth ? (
-                                rol === "admin" ? (
-                                    navigationAuthWithRol.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className={classNames(
-                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                'rounded-md px-3 py-2 text-sm font-medium'
-                                            )}
-                                            aria-current={item.current ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </a>
-                                    ))
-                                ) : (
-                                    navigationAuth.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className={classNames(
-                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                'rounded-md px-3 py-2 text-sm font-medium'
-                                            )}
-                                            aria-current={item.current ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </a>
-                                    ))
-                                )
-                            ) : (
-                                navigationUnauth.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'rounded-md px-3 py-2 text-sm font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))
-                            )} */}
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+
                                     </div>
                                 </div>
                             </div>
-                            {/* {
-                                (auth ? (
-                                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <Menu as="div" className="relative ml-3">
-                                    <div>
-                                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                            <span className="absolute -inset-1.5" />
-                                            <span className="sr-only">Open user menu</span>
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                                                alt=""
-                                            />
-                                        </Menu.Button>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                        <div>
-                                                            <Link className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')} onClick={handleLogout}>Cerrar Sesión</Link>
-                                                        </div>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
-                            </div>
-                                ): null
-                                )
-                            } */}
-                            
+
                         </div>
                     </div>
 
-                    {/* <Disclosure.Panel className="sm:hidden">
+                    <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
-                            {auth?(
-                                navigationAuth.map((item) => (
-                                    <Disclosure.Button
-                                        key={item.name}
-                                        as="a"
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'block rounded-md px-3 py-2 text-base font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </Disclosure.Button>
-                                ))
-                            )
-                            :(
-                                navigationUnauth.map((item) => (
-                                    <Disclosure.Button
-                                        key={item.name}
-                                        as="a"
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'block rounded-md px-3 py-2 text-base font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </Disclosure.Button>
-                                ))
-                            )
 
-                            }
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block rounded-md px-3 py-2 text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+
                         </div>
-                    </Disclosure.Panel> */}
+                    </Disclosure.Panel>
                 </>
             )
             }
         </Disclosure >
     );
 };
-// Navbar.propTypes = {
-//     auth: PropTypes.bool.isRequired,
-//     setAuth: PropTypes.func.isRequired,
-//     rol: PropTypes.string.isRequired,
-//     setRol: PropTypes.func.isRequired,
-// };
+
 export default Navbar;
